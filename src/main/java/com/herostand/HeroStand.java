370c48eff2cc0233c261e445bb990eeecd9302d9
PunctualBoat203 <115333460+PunctualBoat203@users.noreply.github.com>
2026-09-20 02:39:29 -0400

    Add dedicated Hero Stand creative tab

--- src/main/java/com/herostand/HeroStand.java
+++ src/main/java/com/herostand/HeroStand.java
@@ -2,9 +2,8 @@ package com.herostand;
 
 import com.herostand.registry.ModBlockEntities;
 import com.herostand.registry.ModBlocks;
+import com.herostand.registry.ModCreativeTabs;
 import com.herostand.registry.ModItems;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -18,12 +17,6 @@ public final class HeroStand {
         ModBlocks.REGISTER.register(bus);
         ModItems.REGISTER.register(bus);
         ModBlockEntities.REGISTER.register(bus);
-        bus.addListener(this::addCreative);
-    }
-
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
-            event.accept(ModItems.HERO_STAND.get());
-        }
+        ModCreativeTabs.REGISTER.register(bus);
     }
 }
