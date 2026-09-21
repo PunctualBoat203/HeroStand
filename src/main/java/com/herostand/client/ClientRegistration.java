481868cbb39f6d0673eaaebc039e1a2f80b4ff6d
PunctualBoat203 <115333460+PunctualBoat203@users.noreply.github.com>
2026-09-20 21:32:39 -0400

    Purge HeroStand GPU cache on resource reload

--- src/main/java/com/herostand/client/ClientRegistration.java
+++ src/main/java/com/herostand/client/ClientRegistration.java
@@ -2,8 +2,10 @@ package com.herostand.client;
 
 import com.herostand.HeroStand;
 import com.herostand.registry.ModBlockEntities;
+import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -14,5 +16,13 @@ public final class ClientRegistration {
     @SubscribeEvent
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerBlockEntityRenderer(ModBlockEntities.HERO_STAND.get(), HeroStandRenderer::new);
+    }
+
+    @SubscribeEvent
+    public static void registerReloadListeners(RegisterClientReloadListenersEvent event) {
+        event.registerReloadListener(
+                (ResourceManagerReloadListener) resourceManager ->
+                        HeroStandRenderer.clearAllGpuCaches()
+        );
     }
 }
